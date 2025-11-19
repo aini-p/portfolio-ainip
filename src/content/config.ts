0d033ac99df580b54fcbdbf1c749489cef78b8c6
aini-p <aini.h.panda@gmail.com>
2025-11-20 02:25:24 +0900

    hotel

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,27 +1,30 @@
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 const artworksCollection = defineCollection({
-  type: 'content',
+  type: "content",
   // `image`ヘルパーを使うことで、画像最適化が簡単になります
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    publishDate: z.date(),
-    src: image(), // 画像ファイルを直接指定できるように変更
-    patreonUrl: z.union([
-      z.string().url(),      // 有効なURL
-      z.literal("None"),   // "None" という文字列
-      z.literal("")        // 空の文字列
-    ]),
-    patreonEmbedImageUrl: image().optional(),
-    relatedImages: z.array(z.string()).optional(),
-    // SEO
-    description: z.string(),
-    characters: z.array(z.string()),
-    series: z.string(),
-    keywords: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      publishDate: z.date(),
+      src: image(), // 画像ファイルを直接指定できるように変更
+      patreonUrl: z.union([
+        z
+          .string()
+          .url(), // 有効なURL
+        z.literal("None"), // "None" という文字列
+        z.literal(""), // 空の文字列
+      ]),
+      patreonEmbedImageUrl: image().optional(),
+      relatedImages: z.array(z.string()).optional(),
+      // SEO
+      description: z.string(),
+      characters: z.array(z.string()),
+      series: z.string(),
+      keywords: z.array(z.string()),
+    }),
 });
 
 export const collections = {
-  'artworks': artworksCollection,
+  artworks: artworksCollection,
 };
